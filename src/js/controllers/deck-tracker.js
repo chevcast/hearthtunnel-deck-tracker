3b1510a4dff5232331950afd8e27bfb170077912
Alex Ford <alex.ford@codetunnel.com>
2015-05-24 21:57:27 -0600

    wip

--- src/js/controllers/deck-tracker.js
+++ src/js/controllers/deck-tracker.js
@@ -1,3 +1,29 @@
-module.exports = function ($rootScope, $scope) {
+var path = require('path');
+var LogWatcher = require('hearthstone-log-watcher');
+var decksPath = path.join(__dirname, '..', '..', 'data', 'decks');
+
+module.exports = function ($rootScope, $scope, $routeParams, $window, mainWindow) {
   $rootScope.title = "Deck Tracker";
+  $scope.selectedZone = "deck";
+  $scope.friendlyName = "Friendly Player";
+  $scope.opposingName = "Opposing Player";
+  $scope.deckZone = [];
+  $scope.handZone = [];
+  $scope.playZone = [];
+  $scope.graveZone = [];
+  $scope.deck = require(path.join(decksPath, $routeParams.deckFile));
+
+  // Configure the log watcher.
+  var logWatcher = new LogWatcher();
+  logWatcher.on('game-start', function (players) {
+    $scope.$apply(function () {
+      players.forEach(function (player) {
+        $scope[player.team.toLowerCase() + 'Name'] = player.name;
+      });
+      mainWindow.redraw();
+    });
+  });
+  logWatcher.on('zone-change', console.log.bind(console));
+  logWatcher.on('game-over', console.log.bind(console));
+  logWatcher.start();
 };
